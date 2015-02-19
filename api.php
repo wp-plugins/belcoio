18f5f6c35d8f717c6278b99d411b338c95a9a183
belco <belco@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-19 05:46:27 +0000

    tagging version 0.1
    git-svn-id: https://plugins.svn.wordpress.org/belcoio/tags/0.1@1093997 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ api.php
@@ -0,0 +1,24 @@
+<?php
+class Belco_API {
+	
+	public static function post($path, $data) {
+		$secret = get_option('belco_secret');
+		$protocol = BELCO_USE_SSL ? 'https://' : 'http://';
+		
+		$response = wp_remote_post($protocol . BELCO_HOST . $path, array(
+			'method' => 'POST',
+			'body' => json_encode($data),
+			'headers' => array(
+				'Content-Type' => 'application/json',
+				'X-Api-Key' => $secret
+			)
+		));
+
+		if ( is_wp_error( $response ) ) {
+		   return $response->get_error_message();
+		}
+		return true;
+	}
+	
+}
+?>
---
+++ templates/notice.php
@@ -0,0 +1,3 @@
+<div class="update-nag belco-install-notice">
+	<?php _e( 'Connect your Belco account to start receiving calls.', 'wp_belco' ); ?> <a href="<?php echo admin_url('admin.php?page=belco');?>">Connect now</a>.
+</div>
