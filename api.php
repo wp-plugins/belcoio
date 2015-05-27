0dfaaccc95209d4e527a9bd111e00d25e0f0e619
belco <belco@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-27 12:42:21 +0000

    tagging version 0.3.5
    git-svn-id: https://plugins.svn.wordpress.org/belcoio/tags/0.3.5@1168469 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- api.php
+++ api.php
@@ -5,8 +5,9 @@ class Belco_API {
 		$secret = get_option('belco_secret');
 		$protocol = BELCO_USE_SSL ? 'https://' : 'http://';
 		
-		$response = wp_remote_post($protocol . BELCO_HOST . $path, array(
+		$response = wp_remote_post($protocol . BELCO_API_HOST . $path, array(
 			'method' => 'POST',
+			'sslverify' => false,
 			'body' => json_encode($data),
 			'headers' => array(
 				'Content-Type' => 'application/json',
@@ -17,6 +18,13 @@ class Belco_API {
 		if ( is_wp_error( $response ) ) {
 		   return $response->get_error_message();
 		}
+    
+    $body = json_decode($response['body']);
+    
+    if ($body->success === false) {
+      return $body->message;
+    }
+    
 		return true;
 	}
 	
--- templates/settings.php
+++ templates/settings.php
@@ -1,29 +1,31 @@
 <div id="belco-settings" class="wrap">
   <div>
-		<h2>Settings</h2>
+    <h2>Settings</h2>
 
     <form id="belco-settings-form" action="options.php" method="post" class="belco-setup">
       <?php @settings_fields('wp_belco'); ?>
       <?php @do_settings_fields('wp_belco'); ?>
-
-			<table class="form-table">
+      
+      <?php settings_errors(); ?>
+      
+      <table class="form-table">
         <tr valign="top">
-	        <th scope="row">Shop id</th>
-	        <td><input type="text" name="belco_shop_id" value="<?php echo esc_attr( get_option('belco_shop_id') ); ?>" class="regular-text" /></td>
+          <th scope="row">Shop id</th>
+          <td><input type="text" name="belco_shop_id" value="<?php echo esc_attr( get_option('belco_shop_id') ); ?>" class="regular-text" /></td>
         </tr>
        
         <tr valign="top">
-	        <th scope="row">Secret</th>
-	        <td>
-						<input type="text" name="belco_secret" value="<?php echo esc_attr( get_option('belco_secret') ); ?>" class="regular-text" />
-						<p class="description">Your shop id and secret can be found in your Belco.io account, under <strong>Settings</strong> > <strong>Api keys</strong>.</p>
-					</td>
+          <th scope="row">Secret</th>
+          <td>
+            <input type="text" name="belco_secret" value="<?php echo esc_attr( get_option('belco_secret') ); ?>" class="regular-text" />
+            <p class="description">Your shop id and secret can be found in your Belco.io account, under <strong>Settings</strong> > <strong>Api keys</strong>.</p>
+          </td>
         </tr>
-	    </table>
-			
-	    <p class="submit">
-	      <button type="submit" class="button button-primary">Save changes</button>
-	    </p>
-		</form>
-	</div>
+      </table>
+      
+      <p class="submit">
+        <button type="submit" class="button button-primary">Save changes</button>
+      </p>
+    </form>
+  </div>
 </div>
