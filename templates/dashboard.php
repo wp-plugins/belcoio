ced5f6ca226053e96d27a87b8cf719595c5540df
belco <belco@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-16 13:41:41 +0000

    Fix check-in
    git-svn-id: https://plugins.svn.wordpress.org/belcoio/trunk@1287109 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/dashboard.php
+++
@@ -1,9 +0,0 @@
-<div id="belco-container">
-	<iframe src="//<?php echo BELCO_HOST; ?><?php echo $page; ?>" id="belco-frame"></iframe>
-</div>
-<?php if (!$installed) : ?>
-	<form id="belco-install-form" action="options.php" method="post" style="display: none;">
-	  <?php @settings_fields('wp_belco'); ?>
-	  <?php @do_settings_fields('wp_belco'); ?>
-	</form>
-<?php endif; ?>
